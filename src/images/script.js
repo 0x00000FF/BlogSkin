270380c0f8dddf03c5834887edc65a4fb5f83930
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-01-12 02:24:53 +0900

    skin work

---
+++ src/images/script.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict";
+
+    function find_sublist(_this) {
+        return _this.children(".sub_category_list");
+    }
+
+    $(".category_list > li")
+        .hover(
+        function () {
+            let submenu = find_sublist($(this));
+            if (!submenu) return;
+
+            submenu.css("display", "block");
+        },
+        function () {
+            let submenu = find_sublist($(this));
+            if (!submenu) return;
+
+            submenu.css("display", "none");
+        });
+})();
